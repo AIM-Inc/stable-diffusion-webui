c0cd3a395b09922cd51898a67a37398aff59e813
Claudia Cuevas <claudia@knowlabs.co>
2022-12-15 11:31:18 -0800

    update how zoom is calculated

--- modules/zoom/zoom.py
+++ modules/zoom/zoom.py
@@ -6,12 +6,22 @@ def get_img_center(img):
     return (int(w / 2), int(h / 2))
 
 
+def calculate_zoom(zoom):
+    if zoom == 0:
+        return zoom
+    elif zoom == -100:
+        return 0.5
+    elif zoom == 100:
+        return 2
+
+
 # zoom image
 def img_zoom_center(img, zoom):
     w, h = img.size
     x, y = get_img_center(img)
 
-    zoom2 = zoom * 2
+    zoom2 = calculate_zoom(zoom) * 2
+
     left = x - (w / zoom2)
     upper = y - (h / zoom2)
     right = x + (w / zoom2)
